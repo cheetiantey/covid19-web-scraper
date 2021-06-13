b29b4c51798285865c216143b7e176dd2306d5bc
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-06-12 19:25:01 -0500

    Modify file

--- scraper.py
+++ scraper.py
@@ -1,10 +1,46 @@
+# Scrapes the Covid-19 data from "https://www.worldometers.info/"
+# and prints out the data sorted(in descending order) according to the number of cases.
 import requests
 from bs4 import BeautifulSoup
+import texttable as tt
+table = tt.Texttable()
 
-req = requests.get("https://www.geeksforgeeks.org/")
+url = 'https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/'
 
-soup = BeautifulSoup(req.content, "html.parser")
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-res = soup.title
+result = []
 
-print(res.get_text())
+result_iterator = iter(soup.find_all('td'))
+
+while True:
+	try:
+		country = next(result_iterator).text
+		cases = next(result_iterator).text
+		deaths = next(result_iterator).text
+		continent = next(result_iterator).text
+
+		result.append((
+			country,
+			int(cases.replace(',', '')),
+			int(deaths.replace(',', '')),
+			continent
+		))
+
+    # Break when there's no more result
+	except StopIteration:
+		break
+
+# Sort the result by the number of cases
+result.sort(key = lambda row: row[1], reverse = True)
+
+# Add an empty row at the beginning for the headers
+table.add_rows([(None, None, None, None)] + result)
+ 
+table.set_cols_align(('c', 'c', 'c', 'c')) 
+table.header((' Country ', ' Cases ', ' Deaths ', ' Region '))
+
+print("Data can be found on: https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/")
+print()
+print(table.draw())
